5f3d3f65f495af8e03efcf2d2b05027b827966dd
dsubnet0 <dsubnet0@gmail.com>
2023-01-14 15:59:49 -0500

    Parse and get length

--- src/big_audio_file.py
+++ src/big_audio_file.py
@@ -5,8 +5,29 @@ class BigAudioFile() :
 
     def __init__(self, filename: str):
         self.filename = filename
-        self.audio = vlc.MediaPlayer(f'file://{self.filename}')
-        self.length = self.audio.get_length()
+        # self.audio = vlc.MediaPlayer(f'file://{self.filename}')
+        self.media_player = vlc.MediaPlayer()
+        self.media = vlc.Media(self.filename)
+        self.media_player.set_media(self.media)
+        self.media.parse_with_options(0, 0)
+        while (not self.media.is_parsed()):
+            print('Parsing...')
+            self.media_player.play()
+            time.sleep(1)
+        
     
     def play(self):
-        self.audio.play()
+        self.media_player.play()
+    
+    @property
+    def length(self):
+        return self.media_player.get_length()
+
+if __name__ == '__main__':
+    baf = BigAudioFile('hello_detroit.mp3')
+    print(baf)
+    print(baf.length)
+    # baf.play()
+    # time.sleep(1)
+    # print(baf.media_player.get_length())
+    # print(baf.length)
--- tests/test_big_audio_file.py
+++ tests/test_big_audio_file.py
@@ -1,11 +1,13 @@
 from src.big_audio_file import BigAudioFile
+import pytest
 
-def setup():
-    my_baf = BigAudioFile(filename="hello_detroit.mp3")
+@pytest.fixture
+def baf():
+    return BigAudioFile(filename="hello_detroit.mp3")
 
-def test_can_create_bigaudiofile(my_baf):
-    assert type(my_baf) is BigAudioFile
+def test_can_create_bigaudiofile(baf):
+    assert type(baf) is BigAudioFile
 
 
-def test_get_file_length(my_baf):
-    assert my_baf.length == 150
+def test_get_file_length(baf):
+    assert baf.length == 298086
